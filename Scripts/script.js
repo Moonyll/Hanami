7c1e31ac0d55ec251f112a850c299759da849957
Moonyll <s.pouwel@laposte.net>
2019-05-16 16:54:56 +0200

    UP3

--- Scripts/script.js
+++ Scripts/script.js
@@ -24,7 +24,7 @@ function delRow(x) {
     document.getElementById("orders").deleteRow(j);
 } 
 //
-function order(x) {
+function order_sushies(x) {
     //alert("Row index is: " + x.parentNode.parentNode.rowIndex);
     var i = x.parentNode.parentNode.rowIndex; // get index of the row to copy
     var row = document.getElementById("sushies").rows[i]; // find row to copy
@@ -42,16 +42,22 @@ function order(x) {
     var col2 = document.createElement('td'); // create third column node
     var col3 = document.createElement('td'); // create fourth column node
     var col4 = document.createElement('td'); // create fifth column node
+    var col5 = document.createElement('td'); // create sixth column node
+    var col6 = document.createElement('td'); // create sevth column node
     // Append column
     row_order.appendChild(col0); // append 1st column to row
     row_order.appendChild(col1); // append 2nd column to row
     row_order.appendChild(col2); // append 3rd column to row
+    row_order.appendChild(col5);
+    row_order.appendChild(col6);
     row_order.appendChild(col3); // append 4th column to row
     row_order.appendChild(col4); // append 5th column to row
     // Set Values
     col0.innerHTML = select_name;
     col1.innerHTML = select_ref;
-    col2.innerHTML = '<input type="number" name="quantity"  min="1" max="50" value="5" />';
+    col2.innerHTML = '1';
+    col5.innerHTML = '<button type="button" onclick="increase(this)">+</button>';
+    col6.innerHTML = '<button type="button" onclick="decrease(this)">-</button>';
     col3.innerHTML = select_price;
     col4.innerHTML = '<button type="button" onclick="delRow(this)">retirer produit</button>';
     // Copy row created
@@ -59,15 +65,149 @@ function order(x) {
 }
 function total() {
     var table_order = document.getElementById("orders"), total_orders=0;
-    //var total_rows = table_order.rows.length;
-    //for (var i = 2; i < total_rows; i++)
-    //{
-    //total_orders = total_orders + parseFloat((table_order.rows[i].cells[3].innerHTML).replace(",", "."));
-    //}
-    //alert(total_orders);
-    alert(table_order.rows[3].cells[2].value);
+    var total_rows = table_order.rows.length;
+    for (var i = 2; i < total_rows; i++)
+    {
+    total_orders = total_orders + parseFloat((table_order.rows[i].cells[5].innerHTML).replace(",", "."))*parseInt(table_order.rows[i].cells[2].innerHTML);
+    }
+    var round_total = Math.round(total_orders * 100) / 100; // Arrondi à 2 chiffres !
+    document.getElementById("invoice").innerText= "** Merci pour votre commmande ! Votre panier s'élève à : " + round_total+" Euros. **";
 } 
-//function checkTextbox(qty) {
-//    var order_quantity = qty.value;
-//    alert(order_quantity);
-//}
+
+function increase(x) {
+    var k = x.parentNode.parentNode.rowIndex;
+    var table_order = document.getElementById("orders");
+    var inc = parseInt(table_order.rows[k].cells[2].innerHTML, 10);
+    inc = isNaN(inc) ? 0 : inc;
+    inc++;
+    table_order.rows[k].cells[2].innerHTML = inc;
+}
+
+function decrease(x) {
+    var k = x.parentNode.parentNode.rowIndex;
+    var table_order = document.getElementById("orders");
+    var dec = parseInt(table_order.rows[k].cells[2].innerHTML, 10);
+    dec = isNaN(dec) ? 0 : dec;
+    dec--;
+    table_order.rows[k].cells[2].innerHTML = dec;
+}
+
+function order_makies(x) {
+    //alert("Row index is: " + x.parentNode.parentNode.rowIndex);
+    var i = x.parentNode.parentNode.rowIndex; // get index of the row to copy
+    var row = document.getElementById("makies").rows[i]; // find row to copy
+    var table = document.getElementById("orders"); // find table to append to
+    //var clone = row.cloneNode(true); // copy children too
+    //table.appendChild(clone); // add new row to end of table
+    var select_name = row.cells[1].innerHTML; // nom produit
+    var select_ref = row.cells[6].innerHTML; // ref produit
+    var select_price = row.cells[4].innerHTML; // prix produit
+    //alert(select_price);
+    // Creating the row
+    var row_order = document.createElement('tr'); // create row node
+    var col0 = document.createElement('td'); // create column node
+    var col1 = document.createElement('td'); // create second column node
+    var col2 = document.createElement('td'); // create third column node
+    var col3 = document.createElement('td'); // create fourth column node
+    var col4 = document.createElement('td'); // create fifth column node
+    var col5 = document.createElement('td'); // create sixth column node
+    var col6 = document.createElement('td'); // create sevth column node
+    // Append column
+    row_order.appendChild(col0); // append 1st column to row
+    row_order.appendChild(col1); // append 2nd column to row
+    row_order.appendChild(col2); // append 3rd column to row
+    row_order.appendChild(col5);
+    row_order.appendChild(col6);
+    row_order.appendChild(col3); // append 4th column to row
+    row_order.appendChild(col4); // append 5th column to row
+    // Set Values
+    col0.innerHTML = select_name;
+    col1.innerHTML = select_ref;
+    col2.innerHTML = '1';
+    col5.innerHTML = '<button type="button" onclick="increase(this)">+</button>';
+    col6.innerHTML = '<button type="button" onclick="decrease(this)">-</button>';
+    col3.innerHTML = select_price;
+    col4.innerHTML = '<button type="button" onclick="delRow(this)">retirer produit</button>';
+    // Copy row created
+    table.appendChild(row_order); // append row to table
+}
+
+function order_sashimies(x) {
+    //alert("Row index is: " + x.parentNode.parentNode.rowIndex);
+    var i = x.parentNode.parentNode.rowIndex; // get index of the row to copy
+    var row = document.getElementById("sashimies").rows[i]; // find row to copy
+    var table = document.getElementById("orders"); // find table to append to
+    //var clone = row.cloneNode(true); // copy children too
+    //table.appendChild(clone); // add new row to end of table
+    var select_name = row.cells[1].innerHTML; // nom produit
+    var select_ref = row.cells[6].innerHTML; // ref produit
+    var select_price = row.cells[4].innerHTML; // prix produit
+    //alert(select_price);
+    // Creating the row
+    var row_order = document.createElement('tr'); // create row node
+    var col0 = document.createElement('td'); // create column node
+    var col1 = document.createElement('td'); // create second column node
+    var col2 = document.createElement('td'); // create third column node
+    var col3 = document.createElement('td'); // create fourth column node
+    var col4 = document.createElement('td'); // create fifth column node
+    var col5 = document.createElement('td'); // create sixth column node
+    var col6 = document.createElement('td'); // create sevth column node
+    // Append column
+    row_order.appendChild(col0); // append 1st column to row
+    row_order.appendChild(col1); // append 2nd column to row
+    row_order.appendChild(col2); // append 3rd column to row
+    row_order.appendChild(col5);
+    row_order.appendChild(col6);
+    row_order.appendChild(col3); // append 4th column to row
+    row_order.appendChild(col4); // append 5th column to row
+    // Set Values
+    col0.innerHTML = select_name;
+    col1.innerHTML = select_ref;
+    col2.innerHTML = '1';
+    col5.innerHTML = '<button type="button" onclick="increase(this)">+</button>';
+    col6.innerHTML = '<button type="button" onclick="decrease(this)">-</button>';
+    col3.innerHTML = select_price;
+    col4.innerHTML = '<button type="button" onclick="delRow(this)">retirer produit</button>';
+    // Copy row created
+    table.appendChild(row_order); // append row to table
+}
+
+function order_brochettes(x) {
+    //alert("Row index is: " + x.parentNode.parentNode.rowIndex);
+    var i = x.parentNode.parentNode.rowIndex; // get index of the row to copy
+    var row = document.getElementById("brokettes").rows[i]; // find row to copy
+    var table = document.getElementById("orders"); // find table to append to
+    //var clone = row.cloneNode(true); // copy children too
+    //table.appendChild(clone); // add new row to end of table
+    var select_name = row.cells[1].innerHTML; // nom produit
+    var select_ref = row.cells[6].innerHTML; // ref produit
+    var select_price = row.cells[4].innerHTML; // prix produit
+    //alert(select_price);
+    // Creating the row
+    var row_order = document.createElement('tr'); // create row node
+    var col0 = document.createElement('td'); // create column node
+    var col1 = document.createElement('td'); // create second column node
+    var col2 = document.createElement('td'); // create third column node
+    var col3 = document.createElement('td'); // create fourth column node
+    var col4 = document.createElement('td'); // create fifth column node
+    var col5 = document.createElement('td'); // create sixth column node
+    var col6 = document.createElement('td'); // create sevth column node
+    // Append column
+    row_order.appendChild(col0); // append 1st column to row
+    row_order.appendChild(col1); // append 2nd column to row
+    row_order.appendChild(col2); // append 3rd column to row
+    row_order.appendChild(col5);
+    row_order.appendChild(col6);
+    row_order.appendChild(col3); // append 4th column to row
+    row_order.appendChild(col4); // append 5th column to row
+    // Set Values
+    col0.innerHTML = select_name;
+    col1.innerHTML = select_ref;
+    col2.innerHTML = '1';
+    col5.innerHTML = '<button type="button" onclick="increase(this)">+</button>';
+    col6.innerHTML = '<button type="button" onclick="decrease(this)">-</button>';
+    col3.innerHTML = select_price;
+    col4.innerHTML = '<button type="button" onclick="delRow(this)">retirer produit</button>';
+    // Copy row created
+    table.appendChild(row_order); // append row to table
+}
